872dbe12894822ad975fcebb3ba8692f0b63996d
ABHISHEK TIWARI (Frontend-Developer) <tiwariabhishek27611@gmail.com>
2022-10-09 06:24:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import Carrousal from "./Carrousal";
+import "./styles.css";
+
+export default function App() {
+  return <Carrousal />;
+}
---
+++ src/Carrousal.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Carrousal = () => {
+  return (
+    <div className="carrousal-wrapper">
+      <div className="carrousal">
+        <article className="card">
+          <h1>HTML</h1>
+        </article>
+        <article className="card">
+          <h1>CSS</h1>
+        </article>
+        <article className="card">
+          <h1>JAVASCRIPT</h1>
+        </article>
+        <article className="card">
+          <h1>REACTJS</h1>
+        </article>
+        <article className="card">
+          <h1>NEXTJS</h1>
+        </article>
+        <article className="card">
+          <h1>ANGULAR</h1>
+        </article>
+        <article className="card">
+          <h1>VEUJS</h1>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default Carrousal;
